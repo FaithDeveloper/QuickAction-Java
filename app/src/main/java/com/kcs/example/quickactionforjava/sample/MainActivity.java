6963ad9a25cd3df1cf057bae1823ef803909e36a
신규찬 <dev.faithpeople@gmail.com>
2020-04-26 23:58:44 +0900

    샘플 소스 업데이트

--- app/src/main/java/com/kcs/example/quickactionforjava/sample/MainActivity.java
+++ app/src/main/java/com/kcs/example/quickactionforjava/sample/MainActivity.java
@@ -14,12 +14,17 @@ public class MainActivity extends AppCompatActivity {
     private QuickAction mQuickAction; // == popup menu
 
     // action id
-    private static final int ID_DELETE = 1;
+    private static final int ID_ITEM_1 = 1;
+    private static final int ID_ITEM_2 = 2;
+    private static final int ID_ITEM_3 = 3;
+    private static final int ID_ITEM_4 = 4;
+    private static final int ID_ITEM_5 = 5;
     private static final int ID_CANCEL = 99;
 
     private int selectIdx = 0;
 
     private int[] btnResArray = new int[]{R.id.btn_test, R.id.btn_test1, R.id.btn_test2};
+    private int[] itemArray = new int[]{ID_ITEM_1, ID_ITEM_2, ID_ITEM_3, ID_ITEM_4, ID_ITEM_5};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,8 +53,13 @@ public class MainActivity extends AppCompatActivity {
         mQuickAction = new QuickAction(this, QuickAction.VERTICAL);
         mQuickAction.setOnActionItemClickListener(mActionClick);
 
-        ActionItem deletableItem = new ActionItem(ID_DELETE,"삭제", null, selectIdx);
-        mQuickAction.addActionItem(deletableItem); // add action items into QuickAction.
+//        for(int itemIndex : itemArray) {
+//            ActionItem deletableItem = new ActionItem(itemIndex, "ITEM " + (itemIndex), null, selectIdx);
+//            mQuickAction.addActionItem(deletableItem); // add action items into QuickAction.
+//        }
+
+        ActionItem deletableItem = new ActionItem(ID_ITEM_1, "ITEM 1", null, selectIdx);
+            mQuickAction.addActionItem(deletableItem); // add action items into QuickAction.
 
         ActionItem cancelItem = new ActionItem(ID_CANCEL, "취소", null,selectIdx);
         mQuickAction.addActionItem(cancelItem);
