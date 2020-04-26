bbd627e58caca61a1f3a314f2378c82644e06301
신규찬 <dev.faithpeople@gmail.com>
2020-04-26 23:06:08 +0900

    샘플 소스 업데이트

--- app/src/main/java/com/kcs/example/quickactionforjava/sample/MainActivity.java
+++ app/src/main/java/com/kcs/example/quickactionforjava/sample/MainActivity.java
@@ -14,40 +14,48 @@ public class MainActivity extends AppCompatActivity {
     private QuickAction mQuickAction; // == popup menu
 
     // action id
-    private static final int ID_DeletableY = 1;
-    private static final int ID_EditableY = 2;
-    private static final int ID_SNSArchivingY = 3;
-    private static final int ID_SNSArchivingN = 4;
-    private static final int ID_SNSReportedY = 5;
-    private static final int ID_CANCEL = 6;
+    private static final int ID_DELETE = 1;
+    private static final int ID_CANCEL = 99;
 
     private int selectIdx = 0;
+
+    private int[] btnResArray = new int[]{R.id.btn_test, R.id.btn_test1, R.id.btn_test2};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mQuickAction = new QuickAction(this, QuickAction.VERTICAL);
-        // Set listener for action item clicked
-        mQuickAction.setOnActionItemClickListener(mActionClick);
+        init();
+        initOnClickListener();
 
-        findViewById(R.id.btn_test).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                ActionItem deletableItem = new ActionItem(ID_DeletableY,
-                        "삭제", null, selectIdx);
-                mQuickAction.addActionItem(deletableItem); // add action items into QuickAction.
-
-                ActionItem snsCancel = new ActionItem(ID_CANCEL, "취소", null,
-                        selectIdx);
-                mQuickAction.addActionItem(snsCancel);
-
-                mQuickAction.show(v);
-            }
-        });
     }
 
+    /** 클릭 리스너 정의 */
+    private void initOnClickListener(){
+        for(int res : btnResArray){
+            findViewById(res).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    mQuickAction.show(v);
+                }
+            });
+        }
+    }
+
+    /** 초기 세팅 */
+    private void init(){
+        mQuickAction = new QuickAction(this, QuickAction.VERTICAL);
+        mQuickAction.setOnActionItemClickListener(mActionClick);
+
+        ActionItem deletableItem = new ActionItem(ID_DELETE,"삭제", null, selectIdx);
+        mQuickAction.addActionItem(deletableItem); // add action items into QuickAction.
+
+        ActionItem cancelItem = new ActionItem(ID_CANCEL, "취소", null,selectIdx);
+        mQuickAction.addActionItem(cancelItem);
+    }
+
+    /** 퀵 버튼 리스너 */
     private QuickAction.OnActionItemClickListener mActionClick = new QuickAction.OnActionItemClickListener() {
 
         @Override
